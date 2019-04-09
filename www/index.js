47c2b2874d5d0849c0790f717b996b8543ebf473
raju ghorai <39460245+coderj001@users.noreply.github.com>
2019-04-09 17:15:58 +0530

    application
    all the necessary file

---
+++ www/index.js
@@ -0,0 +1,37 @@
+window.addEventListener('load',() =>{
+    const sounds = document.querySelectorAll(".sound");
+    const pads =  document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#17cf17",
+        "#e00e0e",
+        "#b4e70d",
+        "#6d00fc",
+        "#03ece1",
+        "#0509e2"
+    ];
+
+// Lets get going with the sounds
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            // console.log(sounds[index].currentTime)
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+
+            document.querySelector("header").style.color = colors[index];
+        });
+    });
+
+    // create a function that make bubble
+    const createBubbles = (index)=>{
+        const bubble = document.createElement("div")
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    }
+});
